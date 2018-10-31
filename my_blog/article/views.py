b6fdc95957fc2c8d46c49df247f1addb077f2459
lyr1994 <lyray1994@gmail.com>
2018-10-31 14:45:11 -0700

    add article edit

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -40,3 +40,19 @@ def article_delete(request, id):
     article = ArticlePost.objects.get(id=id)
     article.delete()
     return redirect("article:article_list")
+
+def article_update(request, id):
+    article = ArticlePost.objects.get(id=id)
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            article.save()
+            return redirect("article:article_detail", id=id)
+        else:
+            return HttpResponse("Form has Error, please re-enter!")
+    else:
+        article_post_form = ArticlePostForm()
+        context = { 'article': article, 'article_post_form': article_post_form}
+        return render(request, 'article/update.html', context)
