d09bc1228f7fbce40ae7279139877bf9c4df3aa8
lyr1994 <lyray1994@gmail.com>
2018-10-30 19:06:39 -0700

    add article view & add create article view

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from article.models import ArticlePost
+import markdown
+from article.form import ArticlePostForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 def article_list(request):
     articles = ArticlePost.objects.all()
     context = {'articles': articles}
     return render(request, 'article/list.html', context)
+
+def article_detail(request, id):
+    article = ArticlePost.objects.get(id=id)
+
+    article.body = markdown.markdown(article.body, extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+    ])
+    context = {'article': article}
+    return render(request, 'article/detail.html', context)
+
+def article_create(request):
+    if request.method == 'POST':
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
+            new_article.save()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("Form has error, please re-enter!")
+    else:
+        article_post_form = ArticlePostForm()
+        context = {'article_post_form': article_post_form}
+        return render(request, 'article/create.html', context)
